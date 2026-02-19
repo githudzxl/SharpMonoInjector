2974a15ed73f95caaa77e7724772d305203f41f0
githudzxl <59473354+githudzxl@users.noreply.github.com>
2026-02-19 16:12:43 +0800

    Add files via upload

---
+++ SharpMonoInjector - 保留卸载按键（不好用）副本/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace SharpMonoInjector.Gui
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblProcess = new System.Windows.Forms.Label();
+            this.cmbProcesses = new System.Windows.Forms.ComboBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.lblAssembly = new System.Windows.Forms.Label();
+            this.txtAssemblyPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblNamespace = new System.Windows.Forms.Label();
+            this.txtNamespace = new System.Windows.Forms.TextBox();
+            this.lblClassName = new System.Windows.Forms.Label();
+            this.txtClassName = new System.Windows.Forms.TextBox();
+            this.lblMethodName = new System.Windows.Forms.Label();
+            this.txtMethodName = new System.Windows.Forms.TextBox();
+            this.btnInject = new System.Windows.Forms.Button();
+            this.btnEject = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblProcess
+            // 
+            this.lblProcess.AutoSize = true;
+            this.lblProcess.Location = new System.Drawing.Point(12, 15);
+            this.lblProcess.Name = "lblProcess";
+            this.lblProcess.Size = new System.Drawing.Size(53, 13);
+            this.lblProcess.TabIndex = 0;
+            this.lblProcess.Text = "进程:";
+            // 
+            // cmbProcesses
+            // 
+            this.cmbProcesses.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbProcesses.FormattingEnabled = true;
+            this.cmbProcesses.Location = new System.Drawing.Point(71, 12);
+            this.cmbProcesses.Name = "cmbProcesses";
+            this.cmbProcesses.Size = new System.Drawing.Size(200, 21);
+            this.cmbProcesses.TabIndex = 1;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(277, 10);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 2;
+            this.btnRefresh.Text = "刷新";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // lblAssembly
+            // 
+            this.lblAssembly.AutoSize = true;
+            this.lblAssembly.Location = new System.Drawing.Point(12, 45);
+            this.lblAssembly.Name = "lblAssembly";
+            this.lblAssembly.Size = new System.Drawing.Size(53, 13);
+            this.lblAssembly.TabIndex = 3;
+            this.lblAssembly.Text = "程序集:";
+            // 
+            // txtAssemblyPath
+            // 
+            this.txtAssemblyPath.Location = new System.Drawing.Point(71, 42);
+            this.txtAssemblyPath.Name = "txtAssemblyPath";
+            this.txtAssemblyPath.Size = new System.Drawing.Size(200, 20);
+            this.txtAssemblyPath.TabIndex = 4;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(277, 40);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 5;
+            this.btnBrowse.Text = "浏览...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lblNamespace
+            // 
+            this.lblNamespace.AutoSize = true;
+            this.lblNamespace.Location = new System.Drawing.Point(12, 75);
+            this.lblNamespace.Name = "lblNamespace";
+            this.lblNamespace.Size = new System.Drawing.Size(53, 13);
+            this.lblNamespace.TabIndex = 6;
+            this.lblNamespace.Text = "命名空间:";
+            // 
+            // txtNamespace
+            // 
+            this.txtNamespace.Location = new System.Drawing.Point(71, 72);
+            this.txtNamespace.Name = "txtNamespace";
+            this.txtNamespace.Size = new System.Drawing.Size(281, 20);
+            this.txtNamespace.TabIndex = 7;
+            // 
+            // lblClassName
+            // 
+            this.lblClassName.AutoSize = true;
+            this.lblClassName.Location = new System.Drawing.Point(12, 105);
+            this.lblClassName.Name = "lblClassName";
+            this.lblClassName.Size = new System.Drawing.Size(41, 13);
+            this.lblClassName.TabIndex = 8;
+            this.lblClassName.Text = "类名:";
+            // 
+            // txtClassName
+            // 
+            this.txtClassName.Location = new System.Drawing.Point(71, 102);
+            this.txtClassName.Name = "txtClassName";
+            this.txtClassName.Size = new System.Drawing.Size(281, 20);
+            this.txtClassName.TabIndex = 9;
+            // 
+            // lblMethodName
+            // 
+            this.lblMethodName.AutoSize = true;
+            this.lblMethodName.Location = new System.Drawing.Point(12, 135);
+            this.lblMethodName.Name = "lblMethodName";
+            this.lblMethodName.Size = new System.Drawing.Size(53, 13);
+            this.lblMethodName.TabIndex = 10;
+            this.lblMethodName.Text = "方法名:";
+            // 
+            // txtMethodName
+            // 
+            this.txtMethodName.Location = new System.Drawing.Point(71, 132);
+            this.txtMethodName.Name = "txtMethodName";
+            this.txtMethodName.Size = new System.Drawing.Size(281, 20);
+            this.txtMethodName.TabIndex = 11;
+            // 
+            // btnInject
+            // 
+            this.btnInject.Location = new System.Drawing.Point(71, 165);
+            this.btnInject.Name = "btnInject";
+            this.btnInject.Size = new System.Drawing.Size(100, 30);
+            this.btnInject.TabIndex = 12;
+            this.btnInject.Text = "注入";
+            this.btnInject.UseVisualStyleBackColor = true;
+            this.btnInject.Click += new System.EventHandler(this.btnInject_Click);
+            // 
+            // btnEject
+            // 
+            this.btnEject.Enabled = false;
+            this.btnEject.Location = new System.Drawing.Point(192, 165);
+            this.btnEject.Name = "btnEject";
+            this.btnEject.Size = new System.Drawing.Size(100, 30);
+            this.btnEject.TabIndex = 13;
+            this.btnEject.Text = "卸载";
+            this.btnEject.UseVisualStyleBackColor = true;
+            this.btnEject.Click += new System.EventHandler(this.btnEject_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 205);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(41, 13);
+            this.lblStatus.TabIndex = 14;
+            this.lblStatus.Text = "就绪";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 231);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnEject);
+            this.Controls.Add(this.btnInject);
+            this.Controls.Add(this.txtMethodName);
+            this.Controls.Add(this.lblMethodName);
+            this.Controls.Add(this.txtClassName);
+            this.Controls.Add(this.lblClassName);
+            this.Controls.Add(this.txtNamespace);
+            this.Controls.Add(this.lblNamespace);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtAssemblyPath);
+            this.Controls.Add(this.lblAssembly);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.cmbProcesses);
+            this.Controls.Add(this.lblProcess);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SharpMonoInjector";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label lblProcess;
+        private System.Windows.Forms.ComboBox cmbProcesses;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Label lblAssembly;
+        private System.Windows.Forms.TextBox txtAssemblyPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblNamespace;
+        private System.Windows.Forms.TextBox txtNamespace;
+        private System.Windows.Forms.Label lblClassName;
+        private System.Windows.Forms.TextBox txtClassName;
+        private System.Windows.Forms.Label lblMethodName;
+        private System.Windows.Forms.TextBox txtMethodName;
+        private System.Windows.Forms.Button btnInject;
+        private System.Windows.Forms.Button btnEject;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
---
+++ SharpMonoInjector - 保留卸载按键（不好用）副本/Form1.cs
@@ -0,0 +1,234 @@
+﻿using SharpMonoInjector;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SharpMonoInjector.Gui
+{
+    public partial class Form1 : Form
+    {
+        private IntPtr _injectedAssembly = IntPtr.Zero;
+        private const string ConfigFilePath = "inject_config.ini";
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtNamespace.TextChanged += TxtNamespace_TextChanged; // 自动去.dll后缀
+            RefreshProcesses();
+            LoadConfig();
+        }
+
+        // 自动移除命名空间输入框里的 .dll 后缀
+        private void TxtNamespace_TextChanged(object sender, EventArgs e)
+        {
+            string text = txtNamespace.Text.Trim();
+            if (text.EndsWith(".dll", StringComparison.OrdinalIgnoreCase))
+            {
+                txtNamespace.TextChanged -= TxtNamespace_TextChanged;
+                txtNamespace.Text = text.Substring(0, text.Length - 4);
+                txtNamespace.SelectionStart = txtNamespace.Text.Length;
+                txtNamespace.TextChanged += TxtNamespace_TextChanged;
+            }
+        }
+
+        private void RefreshProcesses()
+        {
+            cmbProcesses.DataSource = null;
+            cmbProcesses.Items.Clear();
+
+            List<SharpMonoInjector.ProcessItem> monoProcesses;
+            try
+            {
+                monoProcesses = ProcessUtils.GetMonoProcesses();
+            }
+            catch
+            {
+                monoProcesses = Process.GetProcesses()
+                    .Where(p => !string.IsNullOrEmpty(p.ProcessName))
+                    .OrderBy(p => p.ProcessName)
+                    .Select(p => new SharpMonoInjector.ProcessItem(p.ProcessName, p.Id))
+                    .ToList();
+            }
+
+            cmbProcesses.DataSource = monoProcesses;
+            cmbProcesses.DisplayMember = "DisplayText";
+            cmbProcesses.ValueMember = "Id";
+
+            if (monoProcesses.Count > 0)
+            {
+                cmbProcesses.SelectedIndex = 0;
+            }
+        }
+
+#pragma warning disable IDE1006
+        private void btnRefresh_Click(object sender, EventArgs e)
+#pragma warning restore IDE1006
+        {
+            RefreshProcesses();
+        }
+
+#pragma warning disable IDE1006
+        private void btnBrowse_Click(object sender, EventArgs e)
+#pragma warning restore IDE1006
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Assembly Files (*.dll)|*.dll|All Files (*.*)|*.*";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    txtAssemblyPath.Text = ofd.FileName;
+                }
+            }
+        }
+
+#pragma warning disable IDE1006
+        private void btnInject_Click(object sender, EventArgs e)
+#pragma warning restore IDE1006
+        {
+            try
+            {
+                if (cmbProcesses.SelectedItem == null)
+                {
+                    MessageBox.Show("请选择一个进程", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(txtAssemblyPath.Text) || !File.Exists(txtAssemblyPath.Text))
+                {
+                    MessageBox.Show("请选择有效的程序集文件", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(txtNamespace.Text))
+                {
+                    MessageBox.Show("请输入命名空间", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(txtClassName.Text))
+                {
+                    MessageBox.Show("请输入类名", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(txtMethodName.Text))
+                {
+                    MessageBox.Show("请输入方法名", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                var processItem = (SharpMonoInjector.ProcessItem)cmbProcesses.SelectedItem;
+                byte[] assemblyBytes = File.ReadAllBytes(txtAssemblyPath.Text);
+
+                using (var injector = new Injector(processItem.Id))
+                {
+                    _injectedAssembly = injector.Inject(
+                        assemblyBytes,
+                        txtNamespace.Text,
+                        txtClassName.Text,
+                        txtMethodName.Text);
+                }
+
+                lblStatus.Text = "注入成功！";
+                lblStatus.ForeColor = System.Drawing.Color.Green;
+                btnEject.Enabled = true;
+                SaveConfig();
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = $"注入失败: {ex.Message}";
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+                MessageBox.Show($"注入失败: {ex.Message}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+#pragma warning disable IDE1006
+        private void btnEject_Click(object sender, EventArgs e)
+#pragma warning restore IDE1006
+        {
+            try
+            {
+                if (_injectedAssembly == IntPtr.Zero)
+                {
+                    MessageBox.Show("没有已注入的程序集", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                var processItem = (SharpMonoInjector.ProcessItem)cmbProcesses.SelectedItem;
+
+                using (var injector = new Injector(processItem.Id))
+                {
+                    injector.Eject(
+                        _injectedAssembly,
+                        txtNamespace.Text,
+                        txtClassName.Text,
+                        txtMethodName.Text);
+                }
+
+                _injectedAssembly = IntPtr.Zero;
+                lblStatus.Text = "卸载成功！";
+                lblStatus.ForeColor = System.Drawing.Color.Green;
+                btnEject.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = $"卸载失败: {ex.Message}";
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+                MessageBox.Show($"卸载失败: {ex.Message}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LoadConfig()
+        {
+            try
+            {
+                if (File.Exists(ConfigFilePath))
+                {
+                    var lines = File.ReadAllLines(ConfigFilePath);
+                    foreach (var line in lines)
+                    {
+                        var parts = line.Split('=');
+                        if (parts.Length != 2) continue;
+
+                        switch (parts[0].Trim())
+                        {
+                            case "Namespace":
+                                txtNamespace.Text = parts[1].Trim();
+                                break;
+                            case "ClassName":
+                                txtClassName.Text = parts[1].Trim();
+                                break;
+                            case "MethodName":
+                                txtMethodName.Text = parts[1].Trim();
+                                break;
+                            case "LastAssemblyPath":
+                                if (File.Exists(parts[1].Trim()))
+                                    txtAssemblyPath.Text = parts[1].Trim();
+                                break;
+                        }
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void SaveConfig()
+        {
+            try
+            {
+                List<string> lines = new List<string>
+                {
+                    $"Namespace={txtNamespace.Text.Trim()}",
+                    $"ClassName={txtClassName.Text.Trim()}",
+                    $"MethodName={txtMethodName.Text.Trim()}",
+                    $"LastAssemblyPath={txtAssemblyPath.Text.Trim()}"
+                };
+                File.WriteAllLines(ConfigFilePath, lines);
+            }
+            catch { }
+        }
+    }
+}
